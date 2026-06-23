c5a99bbdf5bf17edb93f4545f389524dcb1287aa
ESTEBANKB <estebankb8@gmail.com>
2026-06-23 14:12:03 -0500

    Delete codigo hasta LOS NPC ,MAPA, ANIMAL directory

--- codigo hasta LOS NPC ,MAPA, ANIMAL/escena_base.py
+++
@@ -1,9 +0,0 @@
-class EscenaBase:
-    def __init__(self):
-        self.activa = False
-    
-    def actualizar(self, teclas):
-        pass
-    
-    def dibujar(self, pantalla):
-        pass 
--- codigo hasta LOS NPC ,MAPA, ANIMAL/recursos/imagenes/caracteres/mapas/mapa1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="mapa" tilewidth="16" tileheight="16" tilecount="2745" columns="61">
- <image source="../mapa.png" width="988" height="724"/>
-</tileset>
